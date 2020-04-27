eb24fd8aec1f99be3031bf598ea2f45cc0d95c4d
gfive <gfive666@163.com>
2020-04-27 21:39:52 +0800

    4.27

---
+++ src/main/java/Afternoon.java
@@ -0,0 +1,10 @@
+import org.apache.commons.lang.StringUtils;
+
+public class Afternoon {
+
+    public static void main(String[] args) {
+        System.out.println(StringUtils.isBlank(""));
+        System.out.println(StringUtils.isBlank("       "));
+        System.out.println(StringUtils.isBlank(null));
+    }
+}
